994df2b642af46b597b1ba9bc597afa2a7a2b450
Tarunna <tarunnasen@gmail.com>
2025-12-24 18:52:40 +0600

    First Version

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    ScrollReveal().reveal(".reveal", {
+    distance: "60px",
+    origin: "bottom",
+    duration: 800,
+    easing: "ease-out",
+    interval: 100,
+    reset: false
+  });
+})
